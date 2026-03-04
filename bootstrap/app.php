d09b0c878ee549117a8a4aee8a98fc7c9cd9dcf1
vicenteflores000 <vichoxd000@gmail.com>
2026-03-04 11:36:28 -0300

    always https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\EnsurePasswordIsChanged;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_ALL);
         $middleware->alias([
             'admin' => AdminMiddleware::class,
             'active.user' => ActiveUserMiddleware::class,
